55f0964ab89439e5d7b7fbc5379a45dbdfd50635
hilmanimaduddin <hilmanimaduddin179@gmail.com>
2023-05-22 00:11:36 +0700

    first commit

---
+++ js/blog.js
@@ -0,0 +1,49 @@
+// let namasiswa1 = "Saiful";
+// let namasiswa2 = "Paijo";
+// let namasiswa3 = "Joko";
+
+// console.log(namasiswa1);
+// console.log(namasiswa2);
+// console.log(namasiswa3);
+
+// array
+// let namasiswa = ["Saiful", "Robby", "Zaini", "Rendi"];
+// console.log(namasiswa);
+// console.log(namasiswa[0]);
+// console.log(namasiswa[2]);
+
+// object
+// let nama = "Badrun";
+// let alamat = "Tulungagung";
+// let umur = 20;
+
+// console.log(nama);
+// console.log(alamat);
+// console.log(umur);
+
+// let dataPersonal1 = {
+//   nama: "Naruto",
+//   alamat: "Konoha",
+//   umur: 21,
+// };
+
+// let dataPersonal2 = {
+//   nama: "Sasuke",
+//   alamat: "Leaf",
+//   umur: 21,
+// };
+
+// console.log(dataPersonal1);
+// console.log(dataPersonal2);
+
+// array of object
+// let dataKamar = [
+//     {
+//         nama: "Naruto",
+//     alamat: "Konoha",}
+// {nama: "Sasuke",
+//     alamat: "Pasir",
+//     }
+// ];
+
+// console.log(dataKamar)
